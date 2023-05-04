0d8bc11860c6482812145efeea752864dffbf764
exam <exam@Bcalab4-m05>
2023-05-04 12:17:50 +0530

    eee

---
+++ CaseToolsExam/src/square_228.java
@@ -0,0 +1,15 @@
+
+public class square_228 {
+	int length;
+	
+	square_228(int length)
+	{
+		this.length=length;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
